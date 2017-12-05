4a9a8e9e0d75488ced51037af35fde7ca19aa414
Kenny Shepard <kshepard@gmail.com>
2017-12-05 01:26:22 -0500

    Switch to sequences for a speed boost

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,9 +1,12 @@
 module Day05 where
 
+import Prelude hiding (length)
+import Data.Sequence
+
 parse :: [String] -> [Int]
 parse = map read
 
-maze :: [Int] -> (Int -> Int) -> Int
+maze :: Seq Int -> (Int -> Int) -> Int
 maze ints getVal = go 0 0 ints
   where
     go ind steps ints'
@@ -11,13 +14,14 @@ maze ints getVal = go 0 0 ints
       | ind >= length ints = steps
       | otherwise          = go newInd (steps + 1) newInts
       where
-        currVal = ints' !! ind
+        currVal = index ints' ind
         newInd  = ind + currVal
         newVal  = getVal currVal
-        newInts = take ind ints' ++ [newVal] ++ drop (ind + 1) ints'
+        newInts = update ind newVal ints'
 
 main :: IO ()
 main = do
-  ints <- parse . lines <$> readFile "input/05.txt"
+  input <- parse . lines <$> readFile "input/05.txt"
+  let ints = fromList input
   print $ maze ints (+1)
   print $ maze ints (\x -> if x >= 3 then x - 1 else x + 1)
