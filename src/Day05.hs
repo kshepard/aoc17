3a73b5be85f1a709f8cb39c9edd68cfa1ecbb89d
Kenny Shepard <kshepard@gmail.com>
2018-01-03 00:19:46 -0500

    Use mutable vectors for day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,27 +1,26 @@
 module Day05 where
 
-import Prelude hiding (length)
-import Data.Sequence
+-- Note: ghci can't take advantage of the fast vectors.
+-- To run fully optimized, do something like the following instead:
+-- runghc --ghc-arg=-fobject-code --ghc-arg=-O2 src/Day05.hs
 
-parse :: [String] -> [Int]
-parse = map read
+import           Control.Monad.ST
+import qualified Data.Vector.Unboxed         as V
+import qualified Data.Vector.Unboxed.Mutable as M
 
-maze :: Seq Int -> (Int -> Int) -> Int
-maze ints getVal = go 0 0 ints
+maze :: V.Vector Int -> (Int -> Int) -> Int
+maze ints getVal = runST $ V.thaw ints >>= go 0 0
   where
-    go ind steps ints'
-      | ind < 0            = steps
-      | ind >= length ints = steps
-      | otherwise          = go newInd (steps + 1) newInts
-      where
-        currVal = index ints' ind
-        newInd  = ind + currVal
-        newVal  = getVal currVal
-        newInts = update ind newVal ints'
+    go ind steps v
+      | ind < 0 || ind >= M.length v = return steps
+      | otherwise = do
+          currVal <- M.read v ind
+          M.write v ind $ getVal currVal
+          go (ind + currVal) (steps + 1) v
 
 main :: IO ()
 main = do
-  input <- parse . lines <$> readFile "input/05.txt"
-  let ints = fromList input
+  input <- map read . lines <$> readFile "input/05.txt"
+  let ints = V.fromList input
   print $ maze ints (+1)
   print $ maze ints (\x -> if x >= 3 then x - 1 else x + 1)
